b248085d0a84056bb3a6e3d441459dd66ee42c01
Gym Freak <guminyj@gmail.com>
2024-01-02 21:21:52 -0500

    this is my readme project that helps generate a read me for you.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# watch-me-play
+# Watch me play 
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+this is a read me generator which is amazing because anyone now can make a readme fast and easy.
+
+# Table of Contents 
+
+##Instsallion ##Usage ##Contributions ##Test ##Quesions
+
+# Installation Instructions 
+
+you will have to download it from my github account. 
+
+## Usage 
+
+all you have to do is answer the quesions when Prompted.
+
+# Contributions 
+
+i had my private tutor help me.
+
+# Test 
+
+ok
+
+# Questions 
+
+you can contact me via my github account @https://github.com/CryptoGymFreak/watch-me-play or my email: james@tmwc.ca
---
+++ generator.js
@@ -0,0 +1,116 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+
+inquirer
+  .prompt([
+    {
+      name: "title",
+      message: "What is the title of your project?",
+      type: "input",
+    },
+    {
+      name: "description",
+      message: "Enter a description of your project",
+      type: "input",
+    },
+    {
+      name: "license",
+      message: "Pick a license!",
+      type: "list",
+      choices: ["Apache 2.0", "MIT", "GNU Gpl 3"],
+    },
+    {
+      name: "tableOfContents",
+      message: "Enter Table of Contents of your project?",
+      type: "input",
+    },
+    {
+      name: "Installation",
+      message: "Enter how we would install your project?",
+      type: "input",
+    },
+    {
+      name: "Usage",
+      message: "Enter how we would Use your project!",
+      type: "input",
+    },
+    {
+      name: "Contributions",
+      message: "Enter who helped with this project?",
+      type: "input",
+    },
+    {
+      name: "Test",
+      message: "Answer the questions of this project!",
+      type: "input",
+    },
+    {
+      name: "Questions",
+      message: "If you need any help with this project?",
+      type: "input",
+    },
+  ])
+
+  .then((answers) => {
+    console.log("THE ANSWER WAS");
+    console.log(answers);
+    let template = "";
+    if (answers) {
+      if (answers.title) {
+        template += `# ${answers.title} \n\n`;
+        if (answers.license === "Apache 2.0") {
+          console.log(answers.license);
+          template += `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n\n`;
+        } else if (answers.license === "MIT") {
+          template += `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n\n`;
+        } else {
+          template += `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n\n`;
+        }
+      }
+
+      if (answers.description) {
+        template += answers.description + "\n\n";
+      }
+
+      if (answers.tableOfContents) {
+        template += "# Table of Contents \n\n";
+        template += answers.tableOfContents + "\n\n";
+      }
+
+      if (answers.Installation) {
+        template += "# Installation Instructions \n\n";
+        template += answers.Installation + " \n\n";
+      }
+
+      if (answers.Usage) {
+        template += "## Usage \n\n";
+        template += answers.Usage + "\n\n";
+      }
+
+      if (answers.Contributions) {
+        template += "# Contributions \n\n";
+        template += answers.Contributions + "\n\n";
+      }
+
+      if (answers.Test) {
+        template += "# Test \n\n";
+        template += answers.Test + "\n\n";
+      }
+
+      if (answers.Questions) {
+        template += "# Questions \n\n";
+        template += answers.Questions;
+      }
+    }
+
+    fs.writeFile("README.md", template, (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("wrote file");
+      }
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
